61288e0a9ff0e0d7b7369772293a155a854cc7a8
khaind <135423123+khazuuman@users.noreply.github.com>
2026-04-23 22:36:11 +0700

    remove manage schedule

--- src/routes/app/mentor/groups/[groupId]/manage-schedule/+page.svelte
+++
@@ -1,6 +0,0 @@
-<!-- +page.svelte -->
-<script lang="ts">
-    import ComingSoon from "../../components/coming-soon.svelte";
-</script>
-
-<ComingSoon title="Manage Group Meeting Schedule" backPath="/app" />
